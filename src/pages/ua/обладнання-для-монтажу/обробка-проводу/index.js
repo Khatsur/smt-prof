c74df7aca37e22382102e27736c288cd9e378a4d
Nikolay <64469555+Khatsur@users.noreply.github.com>
2021-02-18 10:58:03 +0200

    wire final

---
+++ src/pages/ua/обладнання-для-монтажу/обробка-проводу/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import SEO from "../../../../components/seo"
+import Layout from '../../../../containers/layout/layout'
+import Header from '../../../../containers/layout/header/header-three-ua'
+import Footer from '../../../../containers/layout/footer/footer-one-ua'
+import HeroArea from '../../../../containers/index-processing/wire-ua'
+import MachinesWire from '../../../../containers/machines/wire-ua'
+import ContactArea from '../../../../containers/global/contact-area/contact-three-ua'
+import WhatsnewArea from '../../../../containers/machines/whats-new-area-ua'
+ 
+const IndexPage = ({location}) => (
+  <Layout location={location}>
+    <SEO title="Обробка проводу" description="Обробка проводу. Мірне різання та чищення проводу і кабелю. Обтискування терміналів. Обробка коаксіальних та силових кабелів. Маркування проводів" pathname="ua/обладнання-для-монтажу/обробка-проводу" keywords="обробка проводу, різка проводу, різка кабелю, чищення проводу, чищення кабелю, обтискання контактів, пресування, кремпування терміналів, коаксіальний кабель" lang="ua"/>
+    <Header/>
+    <main className="site-wrapper-reveal">
+      <HeroArea/>
+      <MachinesWire />
+      <ContactArea />
+      <WhatsnewArea />
+    </main>
+    <Footer/>
+  </Layout>
+)
+
+export default IndexPage
+ 
---
+++ src/pages/оборудование-для-монтажа/обработка-провода/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import SEO from "../../../components/seo"
+import Layout from '../../../containers/layout/layout'
+import Header from '../../../containers/layout/header/header-three'
+import Footer from '../../../containers/layout/footer/footer-one'
+import HeroArea from '../../../containers/index-processing/wire'
+import MachinesWire from '../../../containers/machines/wire'
+import ContactArea from '../../../containers/global/contact-area/contact-three-ru'
+import WhatsnewArea from '../../../containers/machines/whats-new-area'
+ 
+const IndexPage = ({location}) => (
+  <Layout location={location}>
+    <SEO title="Обработка провода" description="Обработка провода и кабелей. Мерная резка и зачистка проводов. Резка и зачистка кабелей. Опрессовка контактов.  Обработка коаксиальных и силовых кабелей." pathname="оборудование-для-монтажа/обработка-провода" keywords="обработка провода, резка провода, зачистка провода, зачистка кабеля, опрессовка контактов, кремповка контактов, обжимка провдов, обжимка терминалов" lang="ru"/>
+    <Header/>
+    <main className="site-wrapper-reveal">
+      <HeroArea/>
+      <MachinesWire />
+      <ContactArea />
+      <WhatsnewArea />
+    </main>
+    <Footer/>
+  </Layout>
+)
+
+export default IndexPage
+ 
